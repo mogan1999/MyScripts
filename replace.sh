c2e439ad38df1993a569df449f237e3ac4d2fe14
mogan1999 <848137001@qq.com>
2023-11-14 09:48:02 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# 检查/etc/XrayR目录是否存在
-if [ ! -d "/etc/XrayR" ]; then
-  echo "/etc/XrayR directory does not exist."
+# 检查目录是否存在
+if [ ! -d "/etc/wireguard" ]; then
+  echo "directory does not exist."
   exit 1
 fi
 
-# 检查/etc/XrayR/config.yml文件是否存在
-if [ ! -f "/etc/XrayR/config.yml" ]; then
-  echo "/etc/XrayR/config.yml file does not exist."
+# 检查目录下文件是否存在
+if [ ! -f "/etc/wireguard/warp.conf" ]; then
+  echo "file does not exist."
   exit 1
 fi
 
 # 使用sed命令替换字符串
-sed -i 's/showshowway.top/api.ssw.one/g' /etc/XrayR/config.yml
-
+sed -i 's/DNS = 1.1.1.1,8.8.8.8,8.8.4.4,2606:4700:4700::1111,2001:4860:4860::8888,2001:4860:4860::8844/8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844/g' /etc/wireguard/warp.conf
 # 输出替换结果
 echo "Replacement done."
