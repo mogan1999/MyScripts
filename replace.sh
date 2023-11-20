46715e01cbd204259ac654f310b9a9ca12571ddd
mogan1999 <848137001@qq.com>
2023-11-20 16:42:08 +0800

    speed update

--- replace.sh
+++ replace.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 # 检查目录是否存在
-if [ ! -d "/etc/wireguard" ]; then
+if [ ! -d "/etc/XrayR" ]; then
   echo "directory does not exist."
   exit 1
 fi
 
 # 检查目录下文件是否存在
-if [ ! -f "/etc/wireguard/warp.conf" ]; then
+if [ ! -f "/etc/XrayR/config.yml" ]; then
   echo "file does not exist."
   exit 1
 fi
 
 # 使用sed命令替换字符串
-sed -i 's/DNS = 1.1.1.1,8.8.8.8,8.8.4.4,2606:4700:4700::1111,2001:4860:4860::8888,2001:4860:4860::8844/DNS = 8.8.8.8,8.8.4.4,2001:4860:4860::8888,2001:4860:4860::8844/g' /etc/wireguard/warp.conf
+sed -i 's/SpeedLimit: 100/SpeedLimit: 150/g' /etc/XrayR/config.yml
+sed -i 's/SpeedLimit: 50/SpeedLimit: 60/g' /etc/XrayR/config.yml
 # 输出替换结果
 echo "Replacement done."
