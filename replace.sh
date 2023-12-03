fca1503278835592716dd6bfeb49a69df8e29159
mogan1999 <848137001@qq.com>
2023-12-03 17:12:43 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -13,7 +13,7 @@ if [ ! -f "/etc/XrayR/config.yml" ]; then
 fi
 
 # 使用sed命令替换字符串
-sed -i 's/SpeedLimit: 160/SpeedLimit: 200/g' /etc/XrayR/config.yml &&
+sed -i 's/SpeedLimit: 160/SpeedLimit: 300/g' /etc/XrayR/config.yml &&
 sed -i 's/SpeedLimit: 80/SpeedLimit: 100/g' /etc/XrayR/config.yml &&
 sed -i 's/SpeedLimit: 64/SpeedLimit: 80/g' /etc/XrayR/config.yml 
 # 输出替换结果
