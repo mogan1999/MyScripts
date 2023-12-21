a61a00a168e058b64b74b956f9c20eaedb118f1a
mogan1999 <848137001@qq.com>
2023-12-21 10:06:36 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -13,7 +13,7 @@ if [ ! -f "/etc/XrayR/config.yml" ]; then
 fi
 
 # 使用sed命令替换字符串
-sed -i 's/SpeedLimit: 100/SpeedLimit: 0/g' /etc/XrayR/config.yml 
+sed -i 's/SpeedLimit: 120/SpeedLimit: 80/g' /etc/XrayR/config.yml 
 
 # 输出替换结果
 echo "Replacement done."
