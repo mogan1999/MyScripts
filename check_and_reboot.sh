f554926605d315a2b3c5d0739eb4072b50462517
mogan1999 <848137001@qq.com>
2024-03-29 09:51:42 +0800

    Update check_and_reboot.sh

--- check_and_reboot.sh
+++ check_and_reboot.sh
@@ -1,12 +1,12 @@
-[Unit]
-Description=Check network connection and reboot on failure
-After=network.target wg-quick@warp.service
+#!/bin/bash
 
-[Service]
-Type=oneshot
-ExecStart=/usr/local/bin/check_and_reboot.sh
-RemainAfterExit=yes
-StandardOutput=journal
+echo "开始检测网络连接..."
 
-[Install]
-WantedBy=multi-user.target
+# 尝试执行curl命令检测网络连接，设置超时时间为5秒
+# 直接检测网络连接，不再检查WireGuard服务或接口
+if curl -m 5 ip.gs; then
+    echo "网络连接成功，脚本退出。"
+else
+    echo "网络连接失败，准备重启系统..."
+    sudo reboot now
+fi
