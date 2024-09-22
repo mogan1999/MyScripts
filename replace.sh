6ee2ed31f11eea03fffb67086286b7a8b0902394
mogan1999 <848137001@qq.com>
2024-09-23 07:28:50 +0800

    Update replace.sh

--- replace.sh
+++ replace.sh
@@ -13,7 +13,7 @@ if [ ! -f "/etc/XrayR/config.yml" ]; then
 fi
 
 # 先使用sed命令替换
-sed -i 's/SpeedLimit: 300/SpeedLimit: 500/g' /etc/XrayR/config.yml &&
-sed -i 's/SpeedLimit: 600/SpeedLimit: 1000/g' /etc/XrayR/config.yml
+sed -i 's/s-s-w.top/ssw-servers.com/g' /etc/XrayR/config.yml &&
+#sed -i 's/SpeedLimit: 600/SpeedLimit: 1000/g' /etc/XrayR/config.yml
 # 输出替换结果
 echo "Replacement done."
